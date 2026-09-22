dd056361b1714e799a00e176d50e8943c7c50dbe
zibawaa <Alialnabhan3@gmail.com>
2026-09-22 17:37:03 +0100

    Make Atlas seeding resilient to local DNS resolver

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,7 +1,12 @@
 import { ObjectId } from 'mongodb'
+import dns from 'node:dns'
 import { connectDatabase } from '../src/database.js'
 import { lessons } from '../src/data/lessons.js'
 import { teachers } from '../src/data/teachers.js'
+
+// Some Windows DNS adapters refuse Atlas SRV queries. Keep seeding reliable
+// without changing the application connection code used by Render.
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 
 const { client, db } = await connectDatabase()
 try {
